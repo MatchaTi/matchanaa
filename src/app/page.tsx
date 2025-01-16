f9f4e2909823af30b88f8a3f5ae7826c76cbdcdf
MatchaTi <tehmatcha28@gmail.com>
2025-01-16 15:07:43 +0800

    feat: setup layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 export default async function Home() {
   return (
-    <div>Hello</div>
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum odio eaque maiores laudantium magnam similique, iste excepturi sit maxime repudiandae assumenda illo est perspiciatis. Natus, placeat iure. Officiis, quod atque.
+    </div>
   );
 }

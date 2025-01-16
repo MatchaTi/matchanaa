3a0e03ba40a990cd782540846052cb3e7c0d4a69
MatchaTi <tehmatcha28@gmail.com>
2025-01-16 19:36:35 +0800

    chore: update fetch data

--- src/lib/notion.ts
+++ src/lib/notion.ts
@@ -1,3 +1,4 @@
+import { NotionPage } from '@/type/notionType';
 import { Client } from '@notionhq/client'
 import { QueryDatabaseResponse } from '@notionhq/client/build/src/api-endpoints'
 import React from 'react'
@@ -7,10 +8,10 @@ export const notion = new Client({
   auth: process.env.NOTION_TOKEN,
 })
 
-export const fetchPages = React.cache(async () => {
+export const fetchPages = React.cache(async (): Promise<{results: NotionPage[]}> => {
   const response = (await notion.databases.query({
     database_id: process.env.NOTION_DATABASE_ID!,
   })) as QueryDatabaseResponse
 
-  return response.results
+  return {results: response.results as NotionPage[]}
 })
